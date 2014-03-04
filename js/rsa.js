430fe648515c32ed668ab3384be89eccc9c2e006
soote <waffleee@gmail.com>
2014-03-04 06:28:04 -0500

    allow user to input message to be decoded/encoded

--- js/rsa.js
+++ js/rsa.js
@@ -79,8 +79,81 @@ function generatePrime() {
     }
     return 107;
 }
+
 function randomRange(min, max) {
     return min + Math.round((Math.random()*(max-min)));
+}
+
+function encodeMessage(msg) {
+    var encoded = "";
+    for (var i = 0; i < msg.length; i++) {
+        encoded += encodeInput(msg.charAt(i));
+    }
+    return encoded;
+}
+
+function encodeInput(letter) {
+    switch (letter) {
+        case 'a': return "01";
+        case 'b': return "02";
+        case 'c': return "02";
+        case 'd': return "04";
+        case 'e': return "05";
+        case 'f': return "06";
+        case 'g': return "07";
+        case 'h': return "08";
+        case 'i': return "09";
+        case 'j': return "10";
+        case 'k': return "11";
+        case 'l': return "12";
+        case 'm': return "13";
+        case 'n': return "14";
+        case 'o': return "15";
+        case 'p': return "16";
+        case 'q': return "17";
+        case 'r': return "18";
+        case 's': return "19";
+        case 't': return "20";
+        case 'u': return "21";
+        case 'v': return "22";
+        case 'w': return "23";
+        case 'x': return "24";
+        case 'y': return "25";
+        case 'z': return "26";
+        case ' ': return "27";
+    }
+}
+
+function decodeInput(num) {
+    switch (num) {
+        case 1: return 'a';
+        case 2: return 'b';
+        case 2: return 'c';
+        case 4: return 'd';
+        case 5: return 'e';
+        case 6: return 'f';
+        case 7: return 'g';
+        case 8: return 'h';
+        case 9: return 'i';
+        case 10: return 'j';
+        case 11: return 'k';
+        case 12: return 'l';
+        case 13: return 'm';
+        case 14: return 'n';
+        case 15: return 'o';
+        case 16: return 'p';
+        case 17: return 'q';
+        case 18: return 'r';
+        case 19: return 's';
+        case 20: return 't';
+        case 21: return 'u';
+        case 22: return 'v';
+        case 23: return 'w';
+        case 24: return 'x';
+        case 25: return 'y';
+        case 26: return 'z';
+        case 27: return ' ';
+    }
 }
 return {
     test: function() {
@@ -94,7 +167,10 @@ return {
         console.log("p:",p, "q:", q, "n:", n);
         console.log("phi:",phin);
         console.log("e:", e, "d:",d);
-        var msg = "14152716181502120513";
+        var plaintext = "no problem";
+        var msg = encodeMessage(plaintext);
+        console.log(plaintext, msg);
+        //var msg = "14152716181502120513";
         var cipher = encode(msg, e, n, BLOCKSIZE);
         var decoded  = decode(cipher, d, n, BLOCKSIZE);
         console.log("original: ", msg,'\n', "encrypted: ", cipher, '\n', "decoded: ", decoded); 
